e2e91f20973dac0e5f633b5b98793b5df9c567c8
Teila A Garraway <tgarraway08@yahoo.com>
2021-06-12 11:13:24 -0400

    before pull

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-const express = require('express');
-const cors = require('cors');
-const es6Renderer = require('express-es6-template-engine');
+const express = require("express");
+const cors = require("cors");
+const es6Renderer = require("express-es6-template-engine");
 
 const app = express();
 
 // ----------------------------------------------------------------------------
-//                                Middleware                           
+//                                Middleware
 // ----------------------------------------------------------------------------
 app.use(cors());
-app.use(express.static('public'));
+app.use(express.static("public"));
 // body parsing
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded (converts str => json)
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded (converts str => json)
 
 // Configure Template Engine
-app.engine('html', es6Renderer);
-app.set('views', 'templates');
-app.set('view engine', 'html');
+app.engine("html", es6Renderer);
+app.set("views", "templates");
+app.set("view engine", "html");
 
-app.get('/heartbeat', (req, res) => {
-  res.send('I am up');
+app.get("/heartbeat", (req, res) => {
+  res.send("I am up");
 });
 
-app.listen('8080', () => {
+app.listen("8080", () => {
   console.log("running on port http://localhost:8080");
 });
