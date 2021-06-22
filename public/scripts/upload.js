3308117eb5c8882ae2cbb526b8ee3dfcad4308c2
Heggy Castaneda <heggyy@gmail.com>
2021-06-21 22:34:28 -0400

    redirect the route after upload photo

--- public/scripts/upload.js
+++ public/scripts/upload.js
@@ -1,43 +1,35 @@
 // wait for page ready
 document.addEventListener("DOMContentLoaded", () => {
 
-  const renderModal = () => {
-    const message = document.getElementById("message");
-    const messageDiv = document.createElement('div');
-    message.className = "text-center";
-    messageDiv.innerText = "Success! your photo is up!";
-    messageDiv.innerHTML = `
-      <span>
-        Success! your photo is up!
-        <button>close</button>
-      </span>
-    `;
-    messageDiv.addEventListener('click', () => {
-      // alert("Success photos there!");
-      window.location.href = "/photos";
-    });
-    message.appendChild(messageDiv);
-  };
-
   const form = document.getElementById("upload");
 
   form.onsubmit = (e) => {
-  e.preventDefault();
+    e.preventDefault();
 
-  const formdata = new FormData(e.target);
-  const entries = formdata.entries();
-  const data = Object.fromEntries(entries);
+    const formdata = new FormData(e.target);
+    const entries = formdata.entries();
+    const data = Object.fromEntries(entries);
 
-  // See API docs: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-  fetch("/uploadphoto", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify(data), // body data type must match "Content-Type" header
-  });
+    // See API docs: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+    fetch("/uploadphoto", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data), // body data type must match "Content-Type" header
+      })
+      .then(response => {
+        if (response.status === 200) {
+          renderModal("Success! Photo Updated!", "/users/photos");
+        } else {
+          alert("Error creating new photo!  Server Error code: " + response.status +  "Try again.")
+        }
+      })
+      .catch(err => {
+        console.info(err + " url: " + "/users/photos");
+      });
 
-  console.log("POSTed data to /upload");
+    console.log("POSTed data to /uploadphoto");
   };
 });
 
